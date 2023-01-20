c2d9511e6978a1e0d1ac7fa590ee2b6812caca70
Derek Rodriguez <drod2169@gmail.com>
2023-01-20 07:27:04 -0500

    use spread syntax

--- src/layouts/header/nav.tsx
+++ src/layouts/header/nav.tsx
@@ -5,9 +5,7 @@ import NavItem from './nav-item';
 export default function Nav() {
   return (
     <nav class='flex items-center justify-end p-4 bg-gray-800'>
-      <For each={navItems.filter((x) => x.label !== 'Not Found')}>
-        {(item) => <NavItem href={item.href} label={item.label} />}
-      </For>
+      <For each={navItems.filter((x) => x.label !== 'Not Found')}>{(item) => <NavItem {...item} />}</For>
     </nav>
   );
 }
